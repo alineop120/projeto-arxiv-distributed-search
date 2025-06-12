7f70ff6758a428e0b20bdb9d7ff7502b92eed40b
Mateus Tirulli Rozeti <mtirulli17@gmail.com>
2025-06-12 17:07:14 -0300

    Criação de Classes do projeto de busca distribuída
    Criação das Classes:
     - ServerA.java
     - ServerB.java
     - ServerC.java
     - KMPAlgorithm.java

---
+++ src/main/java/br/ucb/arxivdistributed/util/KMPAlgorithm.java
@@ -0,0 +1,9 @@
+package br.ucb.arxivdistributed.util;
+
+public class KMPAlgorithm {
+
+    public static boolean contains(String txt, String pat) {
+        // TODO: Implementar a lógica de busca
+        return false;
+    }
+}

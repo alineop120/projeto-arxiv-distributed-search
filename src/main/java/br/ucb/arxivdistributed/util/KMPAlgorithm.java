3e6e33421594f8cc3c16354183d8248b3a7a946d
Mateus Tirulli Rozeti <mtirulli17@gmail.com>
2025-06-12 21:34:52 -0300

    Implementação do loop de pesquisa principal
    Implementação do loop principal do algoritmo KMP no método contains, que percorre o arquivo

--- src/main/java/br/ucb/arxivdistributed/util/KMPAlgorithm.java
+++ src/main/java/br/ucb/arxivdistributed/util/KMPAlgorithm.java
@@ -3,14 +3,31 @@ package br.ucb.arxivdistributed.util;
 public class KMPAlgorithm {
 
     public static boolean contains(String txt, String pat) {
-        // TODO: Implementar a lógica de busca usando o array LPS
-        return false;
+        int M = pat.length();
+        int N = txt.length();
+
+        int[] lps = computeLPSArray(pat);
+
+        int i = 0; // índice do texto
+        int j = 0; // índice do padrão
+
+        while (i < N) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                i++;
+                j++;
+            }
+
+            if (j == M) {
+                return true; // padrão encontrado
+            }
+        }
+        return false; // padrão não encontrado
     }
 
     private static int[] computeLPSArray(String pat) {
         int M = pat.length();
         int[] lps = new int[M];
-        int len = 0; 
+        int len = 0;
         lps[0] = 0;
         int i = 1;
 

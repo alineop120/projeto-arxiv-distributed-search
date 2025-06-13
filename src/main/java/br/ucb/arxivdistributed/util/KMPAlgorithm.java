bf5d517e4d380f8e58fe7ff096aae7e97f5e8ee0
Mateus Tirulli Rozeti <mtirulli17@gmail.com>
2025-06-12 21:16:52 -0300

    Implementação da lógica para calculo de array
    Desenvolvimento da lógica para preencher e calcular o array LPS

--- src/main/java/br/ucb/arxivdistributed/util/KMPAlgorithm.java
+++ src/main/java/br/ucb/arxivdistributed/util/KMPAlgorithm.java
@@ -3,14 +3,31 @@ package br.ucb.arxivdistributed.util;
 public class KMPAlgorithm {
 
     public static boolean contains(String txt, String pat) {
-        // TODO: Implementar a lógica de busca
+        // TODO: Implementar a lógica de busca usando o array LPS
         return false;
     }
 
     private static int[] computeLPSArray(String pat) {
         int M = pat.length();
         int[] lps = new int[M];
-        
+        int len = 0; 
+        lps[0] = 0;
+        int i = 1;
+
+        while (i < M) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
         return lps;
     }
 }

adf31816e069fc47ca558b2f6a0408da9d49f4cd
Tamilly <116323270+ctamilly@users.noreply.github.com>
2025-06-13 22:41:25 -0300

    adiciona estrutura básica do servidor C

--- src/main/java/br/ucb/arxivdistributed/serverC/ServerC.java
+++ src/main/java/br/ucb/arxivdistributed/serverC/ServerC.java
@@ -1,1 +1,13 @@
-// Código do ServerC
+package br.ucb.arxivdistributed.serverC;
+
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ServerC {
+    private static final ExecutorService executor = Executors.newFixedThreadPool(10);
+
+    public static void main(String[] args) {
+        System.out.println("Servidor C iniciado (esqueleto) na porta " + 6002);
+    }
+}

b5f3cbb0c7049ffcc349462b44477d5b060c320e
Mateus Tirulli Rozeti <129351985+Rozeti@users.noreply.github.com>
2025-06-15 18:24:18 -0300

    Adição do import socket
    Adição do import:
     - import java.net.Socket;

--- src/main/java/br/ucb/arxivdistributed/serverC/ServerC.java
+++ src/main/java/br/ucb/arxivdistributed/serverC/ServerC.java
@@ -7,6 +7,7 @@ import org.json.JSONArray;
 
 import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 

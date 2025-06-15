d5fc0e5d66eaafb5ee1626d484da6332bc307f92
Tamilly <116323270+ctamilly@users.noreply.github.com>
2025-06-14 22:40:06 -0300

    adiciona cache de dados JSON

--- src/main/java/br/ucb/arxivdistributed/serverC/ServerC.java
+++ src/main/java/br/ucb/arxivdistributed/serverC/ServerC.java
@@ -5,9 +5,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ServerC {
+
+    private static final String DATA_FILE = "data/arxiv_part2.json";
+    private static final JSONArray cachedData = SearchUtils.loadJsonFileCached(DATA_FILE);
+    
     private static final ExecutorService executor = Executors.newFixedThreadPool(10);
 
     public static void main(String[] args) {
-        System.out.println("Servidor C iniciado (esqueleto) na porta " + 6002);
+        
     }
 }
